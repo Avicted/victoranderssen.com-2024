4080af3b2a4543482269a2c8996f4f518e7ce87f
Victor Anderssén <victor.anderssen@gmail.com>
2024-08-16 17:00:36 +0300

    New site with Vite, react-ts template

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Victor Anderssén</h1>
+        <p>Hello Sailor!</p>
+        <section>
+          <a href="https://github.com/Avicted/">GitHub</a>
+
+          <br />
+
+          <a href="https://www.linkedin.com/in/victoranderssen/">LinkedIn</a>
+        </section>
+        </div>
+    </>
+  )
+}
+
+export default App
